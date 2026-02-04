a40848b2c7949e58d4496d752d542108a550c599
RonaldChipanaW <ronald.chipana.w@gmail.com>
2026-02-04 18:19:37 -0400

    Actualizando mi proyecto C4

--- test/sum.test.js
+++ test/sum.test.js
@@ -2,10 +2,12 @@ import { describe, it, expect } from 'vitest';
 import { sum } from '../src/sum.js';
 
 describe('sum', () => {
+  
   it('suma dos números', () => {
     expect(sum(2, 3)).toBe(5);
   });
   it('suma mas cero', () => {
     expect(sum(4, 0)).toBe(4);
   });
+
 });
